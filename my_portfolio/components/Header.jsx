218865a1b6203a7b49ae4ea64bd6773db129b4d8
sajinreji03 <sajinreji9@gmail.com>
2025-09-20 15:32:59 +0530

    set up the Navbar and  homepage

---
+++ my_portfolio/components/Header.jsx
@@ -0,0 +1,28 @@
+import { assets } from '@/assets/assets'
+import React from 'react'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <div className=''>
+      <div>
+        <Image src={assets.sajin} alt='' className='rounded-full w-32' />
+      </div>
+      <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+            Hi! I'm Sajin Reji <Image src={assets.hand_icon} alt='' className='w-6' /> </h3>
+
+            <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'> 
+                Full Stack Web Developer
+            </h1>
+            <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+                <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 '>
+                    Contact me <Image src={assets.right_arrow_white} alt='' className=' w-4' /></a>
+
+                 <a href='/SajinReji.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 '>
+                 My Resume <Image src={assets.download_icon} alt='' className=' w-4' /></a>
+            </div>
+    </div>
+  )
+}
+
+export default Header
