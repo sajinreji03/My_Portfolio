7ffbbc76862862cf0e37981526a9a730a9435dfa
sajinreji03 <sajinreji9@gmail.com>
2025-09-24 02:44:03 +0530

    set up the contact section

---
+++ my_portfolio/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 ' >
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Connect With Me</h4>
+       <h2 className='text-center text-5xl font-Ovo  '>Get in Touch</h2>
+       <p className='text-center max-w-3xl mx-auto mt-5 mb-12 font-Ovo'>
+        I would love to hear from you! Whether you have a question, comment, or feedback, just drop me a message using the form below.</p>
+    </div>
+  )
+}
+
+export default Contact
--- my_portfolio/components/Work.jsx
+++ my_portfolio/components/Work.jsx
@@ -18,20 +18,26 @@ const Work = () => {
                 <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 
                 flex items-center justify-between duration-500 group-hover:bottom-7'>
                  <div>
-                    <h2>
+                    <h2 className='font-semibold'>
                         {project.title}
                     </h2>
-                    <p>
+                    <p className='text-sm text-gray-800'>
                         {project.description}
                     </p>
                  </div>
-                 <div>
+                 <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center 
+                 shadow-[2px_2px_0_#000] group-hover:bg-lime-400 transition '>
                     <Image src={assets.send_icon} alt='send icon' className='w-5' />
                  </div>
                 </div>
             </div>
             ))}
        </div>
+
+        <a href='' className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full 
+        py-2 px-8 mx-auto my-12 hover:bg-cyan-100 duration-500'>
+           Show More <Image src={assets.right_arrow_bold} alt='rightarrow' className='w-4' />        </a>
+
     </div>
   )
 }
