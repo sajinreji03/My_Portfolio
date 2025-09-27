e31091b9006b06759291bb23fe54147851e572b5
sajinreji03 <sajinreji9@gmail.com>
2025-09-27 17:00:10 +0530

    updations

--- my_portfolio/tailwind.config.mjs
+++ my_portfolio/tailwind.config.mjs
@@ -4,13 +4,15 @@ const config = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
         colors:{
             lightHover: '#fcf4ff',
             darkHover: '#2a004a',
-            darkTheme: '#11001F'
+            darkTheme: '#11001F',
         },
         fontFamily:{
             Outfit: ["Outfit", "sans-serif"],
